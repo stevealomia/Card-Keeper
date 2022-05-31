46a9e3df78d45edd89f432b922984529f4c8d07f
Mark <mark.n.bacon.jr@gmail.com>
2022-05-31 18:15:13 -0500

    added attributes to serializers

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -2,6 +2,6 @@ class CreditCard < ApplicationRecord
     has_many :favorite_cards
     has_many :users, through: :favorite_cards
 
-    validates :name, :description, :benefits, :annual_fee, :welcome_bonus, :apr, :img_url, presence: true
+    validates :name, :description, :benefits, :annual_fee, :welcome_bonus, :apr, :img_url, :category, :company, presence: true
 
 end
