fb54a1dfe80c95e2b5cb2cd1f479565631752e55
Mark <mark.n.bacon.jr@gmail.com>
2022-06-21 01:46:17 -0400

    final error handling styling

--- client/src/styles/Error.js
+++ client/src/styles/Error.js
@@ -1,43 +1,11 @@
-import styled from "styled-components";
-
-function Error({ children }) {
+const Error = ({ children }) => {
   console.log(children);
   return (
-    <Wrapper>
-      <Alert>!</Alert>
-      <Message>{children}</Message>
-    </Wrapper>
+    <div className="error__wrapper">
+      <span className="error__alert">!</span>
+      <p>{children}</p>
+    </div>
   );
 }
-
-const Wrapper = styled.div`
-  color: white;
-  background-color: red;
-  width: fit-content;
-  border-radius: 6px;
-  display: flex;
-  padding: 10px;
-  margin: 10px;
-  flex-flow: row;
-  align-items: center;
-  justify-content: space-around;
-`;
-
-const Alert = styled.span`
-  background-color: white;
-  color: red;
-  height: 30px;
-  width: 30px;
-  border-radius: 50%;
-  font-weight: bold;
-  display: grid;
-  place-content: center;
-`;
-
-const Message = styled.p`
-  margin: 0;
-  padding: 10px;
-  font-size: 20px;
-`;
 
 export default Error;
